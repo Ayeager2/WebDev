098b728a3b52abe92c65dccd9b3feb5d13de7686
Ayeager2 <annanyeager@gmail.com>
2022-11-18 15:37:31 -0500

    little bit of work
    not sure I like or understand the validity of styled-components(mainly right now the     <ThemeProvider theme={lightTheme}> is not recognizing my style items.)
    .gonna attempt to push through and keep going...

--- YoutubeClone/client/src/App.js
+++ YoutubeClone/client/src/App.js
@@ -1,22 +1,28 @@
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { Menu } from "./components/Menu";
 import { NavBar } from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { darkTheme, lightTheme } from "./utils/Theme";
 
 const Container = styled.div`
   display: flex;
 `;
-const Main = styled.div``;
+const Main = styled.div`
+  flex: 7;
+`;
 const Wrapper = styled.div``;
 
 function App() {
   return (
-    <Container>
-      <Menu />
-      <Main>
-        <NavBar />
-        <Wrapper>Video Cards</Wrapper>
-      </Main>
-    </Container>
+    <ThemeProvider theme={lightTheme}>
+      <Container>
+        <Menu />
+        <Main>
+          <NavBar />
+          <Wrapper>Video Cards</Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
