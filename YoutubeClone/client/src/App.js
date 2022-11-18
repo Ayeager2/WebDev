4fb5e42be49d448bd4e00c714795602622c8cbb4
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-11-18 01:21:15 -0500

    client start

---
+++ YoutubeClone/client/src/App.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { Menu } from "./components/Menu";
+import { NavBar } from "./components/NavBar";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Main = styled.div``;
+const Wrapper = styled.div``;
+
+function App() {
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <NavBar />
+        <Wrapper>Video Cards</Wrapper>
+      </Main>
+    </Container>
+  );
+}
+
+export default App;
