2e22c6f94cefc461a757cde5be42546d6e288c70
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-11-22 21:42:34 -0500

    auth work

--- YoutubeClone/api/index.js
+++ YoutubeClone/api/index.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/users.js";
 import videoRoutes from "./routes/videos.js";
 import commentsRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -20,12 +21,23 @@ const connect = () => {
     });
 };
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth/", authRoutes);
 app.use("/api/comments/", commentsRoutes);
 app.use("/api/users/", userRoutes);
 app.use("/api/videos/", videoRoutes);
+
+app.use((err,req,res,next)=>{
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success:false,
+    status: status,
+    message: message
+  });
+})
 
 app.listen(8800, () => {
   connect();
--- YoutubeClone/api/routes/users.js
+++ YoutubeClone/api/routes/users.js
@@ -3,4 +3,18 @@ import {} from "../controllers/user.js";
 
 const router = express.Router();
 
+//update user
+
+//delete user
+
+//get a user
+
+//subscribe user
+
+//unsubscribe user
+
+//like video
+
+//dislike video
+
 export default router;
