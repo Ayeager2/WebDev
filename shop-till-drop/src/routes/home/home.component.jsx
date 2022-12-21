b051dff2ef9da4f8f40ae72b438ee34a34f26a05
Ayeager2 <annanyeager@gmail.com>
2022-12-21 09:46:08 -0500

    bit o' work

---
+++ shop-till-drop/src/routes/home/home.component.jsx
@@ -0,0 +1,38 @@
+import Directory from "./../../components/directory/directory.component";
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      subtitle: "Show Now",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      subtitle: "Show Now",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      subtitle: "Show Now",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      subtitle: "Show Now",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      subtitle: "Show Now",
+    },
+  ];
+  return <Directory categories={categories} />;
+};
+
+export default Home;
