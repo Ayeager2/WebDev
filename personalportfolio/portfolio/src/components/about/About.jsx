c96fd12ef1dcf33da68a4ce636ba44d699e7f366
Ayeager2 <annanyeager@gmail.com>
2023-03-23 15:42:11 -0400

    nav bar and about me work

--- personalportfolio/portfolio/src/components/about/About.jsx
+++ personalportfolio/portfolio/src/components/about/About.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "./about.css";
-
+import ME from "../../assets/me.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
-    return <div>About</div>;
+    return (
+        <section id="about">
+            <h5>Get to Know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={ME} alt="" className="about__me" />
+                    </div>
+                </div>
+                <div className="about__content">
+                    <div className="about__cards">
+                        <div className="about__card">
+                            <FaAward className="about__icon" />
+                            <h5>Experience</h5>
+                            <small>2+ Years Working </small>
+                        </div>
+                        <div className="about__card">
+                            <FiUsers className="about__icon" />
+                            <h5>Large Company</h5>
+                            <small>With Many Clients</small>
+                        </div>
+                        <div className="about__card">
+                            <VscFolderLibrary className="about__icon" />
+                            <h5>Always Working On</h5>
+                            <small>Many Personal Projects</small>
+                        </div>
+                    </div>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Ut in debitis possimus ipsum officiis, alias
+                        praesentium error ipsam reiciendis odit optio minima
+                        pariatur voluptate eius consequatur, laudantium dolor ea
+                        ducimus?
+                    </p>
+                    <a href="#contact" className="btn btn-primary">
+                        Let's Talk{" "}
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default About;
--- personalportfolio/portfolio/src/components/nav/Nav.jsx
+++ personalportfolio/portfolio/src/components/nav/Nav.jsx
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 import { BiBook, BiMessageSquareDetail } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState("#");
     return (
         <nav>
-            <a href="#">
+            <a
+                href="#home"
+                onClick={() => setActiveNav("#home")}
+                className={activeNav === "#home" ? "active" : ""}
+            >
                 <AiOutlineHome />
             </a>
-            <a href="#about">
+            <a
+                href="#about"
+                className={activeNav === "#about" ? "active" : ""}
+                onClick={() => setActiveNav("#about")}
+            >
                 <AiOutlineUser />
             </a>
-            <a href="#experience">
+            <a
+                href="#experience"
+                className={activeNav === "#experience" ? "active" : ""}
+                onClick={() => setActiveNav("#experience")}
+            >
                 <BiBook />
             </a>
-            <a href="#service">
+            <a
+                href="#service"
+                className={activeNav === "#service" ? "active" : ""}
+                onClick={() => setActiveNav("#service")}
+            >
                 <RiServiceLine />
             </a>
-            <a href="#contact">
+            <a
+                href="#contact"
+                className={activeNav === "#contact" ? "active" : ""}
+                onClick={() => setActiveNav("#contact")}
+            >
                 <BiMessageSquareDetail />
             </a>
         </nav>
