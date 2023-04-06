b7563e3cc72ffe03ce8a9514089ab6a64b518da7
Ayeager2 <annanyeager@gmail.com>
2023-04-06 14:05:45 -0400

    udpates.
    added swiper and updated portfolio section and testimonial section. started on the contact part

--- personalportfolio/v2/src/components/contact/Contact.tsx
+++ personalportfolio/v2/src/components/contact/Contact.tsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "./contact.css";
+import { HiOutlineMail } from "react-icons/hi";
+import { BiMessageAltDots } from "react-icons/bi";
+
+const data = [
+    {
+        id: 1,
+        icon: <HiOutlineMail />,
+        title: "Email",
+        text: "annyeager@gmail.com",
+        link: "mailto:ayeager@gmail.com",
+        linkMessage: "Email Me",
+    },
+    {
+        id: 1,
+        icon: <BiMessageAltDots />,
+        title: "Message",
+        text: "Linked In",
+        link: "mailto:ayeager@gmail.com",
+        linkMessage: "Notify Me",
+    },
+];
 
 const contact = () => {
-    return <section id="contact">Contact</section>;
+    return (
+        <section id="contact">
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="conatiner contact__container">
+                <div className="contact__options">
+                    {data.map(
+                        ({ id, icon, title, text, link, linkMessage }) => {
+                            return (
+                                <article key={id} className="contact__option">
+                                    {icon}
+                                    <h4>{title}</h4>
+                                    <h5>{text}</h5>
+                                    <a href={link}>{linkMessage}</a>
+                                </article>
+                            );
+                        }
+                    )}
+                </div>
+                {/* END OF CONTACT OPTIONS */}
+                <form action=""></form>
+            </div>
+        </section>
+    );
 };
 
 export default contact;
--- personalportfolio/v2/src/components/testimonials/Testimonials.tsx
+++ personalportfolio/v2/src/components/testimonials/Testimonials.tsx
@@ -1,8 +1,68 @@
 import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import required modules
+import { Pagination } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
 import "./testimonials.css";
 
+import avatar1 from "../../assets/avatar1.jpg";
+import avatar2 from "../../assets/avatar2.jpg";
+import avatar3 from "../../assets/avatar3.jpg";
+import avatar4 from "../../assets/avatar4.jpg";
+
+const data = [
+    {
+        avatar: avatar1,
+        name: "Jane Doe",
+        review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum reiciendis suscipit optio!",
+    },
+    {
+        avatar: avatar2,
+        name: "John Doe",
+        review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum reiciendis suscipit optio!",
+    },
+    {
+        avatar: avatar3,
+        name: "Bob Doe",
+        review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum reiciendis suscipit optio!",
+    },
+    {
+        avatar: avatar4,
+        name: "Lyann Doe",
+        review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum reiciendis suscipit optio!",
+    },
+];
 const Testimonials = () => {
-    return <section id="testimonials">Testimonials</section>;
+    return (
+        <section id="testimonials">
+            <h5>Review from clients</h5>
+            <h2>Testimonials</h2>
+
+            <Swiper
+                className="container testimonials__container"
+                modules={[Pagination]}
+                spaceBetween={40}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+            >
+                {data.map(({ avatar, name, review }, index) => (
+                    <SwiperSlide key={index} className="testimonial">
+                        <div className="client__avatar">
+                            <img src={avatar} alt={name} />
+                        </div>
+                        <h5 className="client__name">{name}</h5>
+                        <small className="client__review">{review}</small>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </section>
+    );
 };
 
 export default Testimonials;
